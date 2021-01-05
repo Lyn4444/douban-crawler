18abdd28e608632b37bd89c07401ed737565d7ea
Lyn4444 <45141851+Lyn4444@users.noreply.github.com>
2021-01-05 18:28:52 +0800

    upgrade

--- shortComments.py
+++ shortComments.py
@@ -5,7 +5,12 @@
 # @Date: 2021/1/5 12:01
 # @IDE: PyCharm
 # @Version: 1.0
-# @Function:
+# @Function: 获取豆瓣电影短评
+
+from selenium import webdriver
+import pandas as pd
 
 
-
+URL = 'https://movie.douban.com/subject/26266893/comments?status=P'
+driver = webdriver.Chrome()
+driver.get(URL)
